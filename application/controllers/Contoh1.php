2749952fe562da29470eb2a752c4f5eabfb9f34b
moudydinamoria <putrimoudy04@gmail>
2023-10-07 13:17:57 +0700

    Pustaka Booking

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<center><h1>HTML (HyperText Markup Language)</h1></center>";
+        echo "HTML adalah bahasa markup standar yang digunakan untuk membuat halaman website dan aplikasi web.
+                Eksistensi file HTML adalah .html atau .htm, yang bisa dilihat dengan menggunakan web browser apa pun
+                (seperti Google Chrome, Safari, atau Mozila Firefox). Browser tersebut membaca file HTML dan merender
+                kontennya sehingga user internet bisa melihat dan membacanya.";
+    }
+}
